730aee811b74b66a1d43d1d5e6090794ce4a3283
Rohit Jain <mail@rohitja.in>
2017-11-06 07:25:00 +0000

    Remove gi notifications

--- openpyn/management/management.py
+++ openpyn/management/management.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-import gi
 import socket
 import os
 import sys
 from time import sleep
 from openpyn import root
-gi.require_version('Notify', '0.7')
-from gi.repository import Notify
-
 
 def socket_connect(server, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,7 +14,6 @@ def socket_connect(server, port):
 
 def show():
     sleep(1)
-    Notify.init("openpyn")
 
     while True:
         try:
@@ -28,10 +23,6 @@ def show():
             continue
         break
     try:
-        # Create the notification object and show once
-        notification = Notify.Notification.new(
-                "Openpyn", "Initiating connection (If stuck here, try again)")
-        notification.show()
         server_name = ""
         last_status_UP = False
         while True:
@@ -47,9 +38,6 @@ def show():
                 last_status_UP = False
 
                 # print ('Received A DOWN', data_str)
-                notification.update("Openpyn", "Connection Down, Disconnected.")
-                # Show again
-                notification.show()
 
             server_name_location = data_str.find("common_name=")
             # print(server_name_location)
@@ -57,21 +45,14 @@ def show():
                 server_name_start = data_str[server_name_location + 12:]
                 server_name = server_name_start[:server_name_start.find(".com") + 4]
                 # print("Both True and server_name", server_name)
-                notification.update("Openpyn", "Connected! to " + server_name)
-                # Show again
-                notification.show()
 
             # break of data stream is empty
             if not data:
                 break
 
     except (KeyboardInterrupt) as err:
-        notification.update("Openpyn", "Disconnected, Bye.")
-        notification.show()
         print('\nShutting down safely, please wait until process exits\n')
     except ConnectionResetError:
-        notification.update("Openpyn", "Disconnected, Bye. (ConnectionReset)")
-        notification.show()
         sys.exit()
 
     s.close()
